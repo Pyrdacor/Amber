9e887b07da62c186ed17277bdd27199c2d07497b
Pyrdacor <trobt@web.de>
2024-08-17 13:21:16 +0200

    Prepare legacy gamedata loader redactor

---
+++ Amberstar.GameData.Legacy/LegacyAssetProvider.cs
@@ -0,0 +1,18 @@
+namespace Amberstar.GameData.Legacy;
+
+public enum LegacyPlatform
+{
+    Atari,
+    Amiga,
+    //Dos // TODO: unsupported for now
+}
+
+public enum EmbeddedDataFile
+{
+    Graphics
+}
+
+public sealed class LegacyAssetProvider
+{
+
+}
