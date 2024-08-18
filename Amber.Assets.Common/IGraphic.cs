63b3d7c7cb343ddf2486d1f524b38a6be041d95e
Pyrdacor <trobt@web.de>
2024-08-18 09:38:04 +0200

    Update IGraphic.cs

--- Amber.Assets.Common/IGraphic.cs
+++ Amber.Assets.Common/IGraphic.cs
@@ -1,9 +1,32 @@
 ﻿namespace Amber.Assets.Common;
 
+public enum GraphicFormat
+{
+    Planar1Bit,
+    Planar3Bit,
+    Planar4Bit,
+    Planar5Bit,
+    R8G8B8A8,
+    X4R4G4B4, // Palettes
+}
+
+public static class GraphicFormatExtensions
+{
+    public static int BitsPerPixel(this GraphicFormat format) => format switch
+    {
+	GraphicFormat.Planar3Bit => 3,
+	GraphicFormat.Planar4Bit => 4,
+	GraphicFormat.Planar5Bit => 5,
+	GraphicFormat.X4R4G4B4 => 16,
+	_ => 32
+    };
+}
+
 public interface IGraphic
 {
 	int Width { get; }
 	int Height { get; }
+	GraphicFormat Format { get; }
 	bool UsePalette { get; }
 	byte[] GetPixelData();
 }
