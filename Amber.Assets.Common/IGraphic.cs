794af63f8518eb3ea8803c1491e5ee16861b1f23
Pyrdacor <trobt@web.de>
2024-08-18 09:52:51 +0200

    Update IGraphic.cs

--- Amber.Assets.Common/IGraphic.cs
+++ Amber.Assets.Common/IGraphic.cs
@@ -2,31 +2,22 @@
 
 public enum GraphicFormat
 {
-    Planar1Bit,
-    Planar3Bit,
-    Planar4Bit,
-    Planar5Bit,
-    R8G8B8A8,
-    X4R4G4B4, // Palettes
+    PaletteIndices,
+    RGBA,
 }
 
 public static class GraphicFormatExtensions
 {
-    public static int BitsPerPixel(this GraphicFormat format) => format switch
+    public static int BytesPerPixel(this GraphicFormat format) => format switch
     {
-	GraphicFormat.Planar1Bit => 1,
-	GraphicFormat.Planar3Bit => 3,
-	GraphicFormat.Planar4Bit => 4,
-	GraphicFormat.Planar5Bit => 5,
-	GraphicFormat.X4R4G4B4 => 16,
-	_ => 32
+	GraphicFormat.PaletteIndices => 1,
+	_ => 4
     };
 
     public static bool UsesPalette(this GraphicFormat format) => format switch
     {
-	GraphicFormat.X4R4G4B4 => false,
-	GraphicFormat.R8G8B8A8 => false,
-	_ => true
+	GraphicFormat.PaletteIndices => true,
+	_ => false
     };
 }
 
